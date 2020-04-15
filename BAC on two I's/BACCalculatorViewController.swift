818fe5cbb2eedaebfbab68ff8e0fa4ef07218130
Elliott Tuttle <s530028@nwmissouri.edu>
2020-04-15 14:59:07 -0500

    Almost Done
    Got all the code to due the calculations, but I cannot figure out the initializers for the variables for them to work.

--- BAC on two I's/BACCalculatorViewController.swift
+++ BAC on two I's/BACCalculatorViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class BACCalculatorViewController: UIViewController {
     
+    private var weight:Double = 0.0
+    private var beer:Double = 0.0
+    private var wine:Double
+    private var liquor:Double
+    private var hours:Double
+    private var minutes:Double
+    private var numResult:String
+    
+    private var eliminationRate = 0.00017
+    private var timeConsuming:Double
+    private var gender:Double = 0.0
+    private var bac:Double = 0.0
+    
     /// All text fields to collect data for BAC
     @IBOutlet weak var weightTF: UITextField!
     @IBOutlet weak var beerTF: UITextField!
@@ -19,16 +32,49 @@ class BACCalculatorViewController: UIViewController {
     @IBOutlet weak var minutesTF: UITextField!
     @IBOutlet weak var resultTF: UITextField!
     
+    
+    @IBAction func maleButton(sender: UIButton){
+        gender = 3.75
+    }
+    @IBAction func femaleButton(sender: UIButton){
+        gender = 4.7
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
     }
     
+//    init(){
+//        weight = Double(weightTF.text)
+//        beer = Double(beerTF.text)
+//        wine = Double(wineTF.text)
+//        liquor = Double(liquorTF.text)
+//        hours = Double(hoursTF.text)
+//        minutes = Double(minutesTF.text)
+//    }
+    
     /// calculate to calculate the BAC
     /// - Parameter sender: UIButton
     @IBAction func calculate(sender: Any){
-        resultTF.text = "10.4 ~ sobriety"
+//        if let text = self.resultTF.text{
+//            var numResult = Double(text)
+        
+        if gender == 4.7{
+            
+                bac = ((beer + wine + liquor) * gender * eliminationRate * timeConsuming)/weight
+            numResult = String(bac)
+            resultTF.text = numResult
+        }
+            
+        else if gender == 3.75{
+            
+                 bac = ((beer + wine + liquor) * gender * eliminationRate * timeConsuming)/weight
+            numResult = String(bac)
+            resultTF.text = numResult
+            }
+        
     }
     
     /// clear to reset all fields
@@ -41,6 +87,7 @@ class BACCalculatorViewController: UIViewController {
         hoursTF.text = ""
         minutesTF.text = ""
         resultTF.text = ""
+        gender = 0.0
     }
     
 }
